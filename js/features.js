0f10de54af37f9a13f87404a5d5da220bf796d5a
rfammon <rafael.ammon@gmail.com>
2025-12-01 18:15:32 -0300

    Update features.js

--- js/features.js
+++ js/features.js
@@ -417,8 +417,8 @@ export async function handleAddTreeSubmit(event) {
             num = parseFloat(num.toFixed(6));
 
             // 2. Limitar o valor absoluto para ser menor que 1000
-            if (Math.abs(num) >= 1000) {
-                console.warn(`Atenção: O valor ${coordValue} excede o limite de 1000. Ajustando.`);
+            if (Math.abs(num) >= 100000) {
+                console.warn(`Atenção: O valor ${coordValue} excede o limite de 100000. Ajustando.`);
                 num = num > 0 ? 999.999999 : -999.999999;
             }
             return num;
